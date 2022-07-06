f596dc024b2303d119e4d1379fb0b72f50fa670c
elo pascon <elopascon@gmail.com>
2022-07-06 13:20:02 -0300

    Tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -2,20 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: 'app-tema',
+  templateUrl: './tema.component.html',
+  styleUrls: ['./tema.component.css']
 })
-export class InicioComponent implements OnInit {
+export class TemaComponent implements OnInit {
 
   constructor(
     private router: Router
   ) { }
 
   ngOnInit(): {
-
-    if( enviroment.token == '')
+    if(environment.token == '')
     this.router.navigate(['/entrar'])
+  }
   }
 
 }
